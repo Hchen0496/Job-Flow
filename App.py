fceaf37ff86abb15c3db0a8b698574a6e1af7a8b
Hchen0496 <>
2021-11-11 17:16:47 +0000

    Finished Creating Tabs

--- App.py
+++ App.py
@@ -2,39 +2,55 @@ import tkinter as tk
 from tkinter import ttk #Getting Tabs from this library
 
 class TabsControl(tk.Frame):
-   def __init__(self, parent):
-        self.parent = parent
-        tk.Frame.__init__(self, self.parent)
-        self.tabControl = ttk.Notebook(self)
-        self.tabs1 = ttk.Frame(self.tabControl)
-        self.tabControl.add(self.tabs1, text ="Main Record")
-        self.parent.labelA = ttk.Label(self, text = "This is on Frame One")
-        self.parent.labelA.grid(column=1, row=1)
+  def __init__(self, parent):
+    self.parent = parent
+    tk.Frame.__init__(self, self.parent)
+    self.tabControl = ttk.Notebook(self.parent) #Initializing Tabs
+    self.style = ttk.Style(self) #initializing the Styling of the Tabs
+    self.add_Tabs() #Calling in the Function of adding Tabs
+    self.Tabs1() #Calling in the function of the First Tab
+    self.Tabs2() #Calling in the function of the Second Tab
+  
+  def add_Tabs(self):
+    #We initialize Tab 1 & 2
+    self.tabs1 = ttk.Frame(self.tabControl)
+    self.tabs2 = ttk.Frame(self.tabControl)
+    #from the Parent of the Switching Tab, we add in Tab 1 & 2, and then we name each of them
+    self.tabControl.add(self.tabs1, text ="Main Record")
+    self.tabControl.add(self.tabs2, text ="Add New Record")
+    #Made sure we pack the parent tab and add arguments of how we want it to appear on windows
+    self.tabControl.pack(expand=True, fill = 'both', padx = 10, pady = 10)
+    
+  def Tabs1(self):
+    self.style.configure('TNotebook.Tab', width = 10, bg="black", borderwidth = 1)#width=self.winfo_screenwidth())
 
-       
+    #self.parent.labelA = ttk.Label(self, text = "This is on Frame One")#self.parent.labelA.grid(column=1, row=1)
+  
+  def Tabs2(self):
+    self.style.configure('TNotebook.Tab', width = 15, height = 520)  
+      
 class MainApplication(tk.Frame):
-
     #Initializing the start of the Window
     def __init__(self, parent):
         self.parent = parent
         tk.Frame.__init__(self, self.parent)
         self.configure_Gui()
-        self.TabControls = TabsControl(self)
-        self.TabControls.pack(expand=1, fill = 'both')
-        #self.Widgets()
+        self.Widgets()
         
     #Windows
     def configure_Gui(self):
         self.parent.title("JobSeeker Organizer") #Window Name
-        self.parent.iconphoto(True, tk.PhotoImage(file='O:\Python\Job Seeker Organization\Images\Icon_Organizer.png')) #Window Icon set to True applies to all windows
+        #self.parent.iconphoto(True, tk.PhotoImage(file='..\Images\Icon_Organizer.png')) #Window Icon set to True applies to all windows
         self.parent.minsize(500,400)
-        #self.parent.bg("ADD8E6")
+        #self.config(bg = "#add8e6")
 
-    #def Widgets(self):
+    def Widgets(self):
+      self.TabControls = TabsControl(self.parent)
+      self.TabControls.pack()
 
-        
 #To return the result
 if __name__ == "__main__":
     root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    App = MainApplication(root)
+    #MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop() #Infinite Loop
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JobSeeker-App
+
+**Updates**
+- Adding in a Tab for work-in-progress (11/10/2021)
+
+**Documentations:**
+- Imports from tkinter
+
+**How to Start the App**
+- In-Progress will be done as .exe instead of .py for client standalone
+
+
