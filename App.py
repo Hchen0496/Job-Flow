f1fb8de6f1bcedd467c91b9cc575be7d506e8f7d
Lynn <Huilin.chen24@gmail.com>
2021-11-10 23:02:35 -0500

    In-Progress of Tabs

--- App.py
+++ App.py
@@ -1,25 +1,40 @@
 import tkinter as tk
-    
+from tkinter import ttk #Getting Tabs from this library
+
+class TabsControl(tk.Frame):
+   def __init__(self, parent):
+        self.parent = parent
+        tk.Frame.__init__(self, self.parent)
+        self.tabControl = ttk.Notebook(self)
+        self.tabs1 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tabs1, text ="Main Record")
+        self.parent.labelA = ttk.Label(self, text = "This is on Frame One")
+        self.parent.labelA.grid(column=1, row=1)
+
+       
 class MainApplication(tk.Frame):
 
-    #Initializing the start of the Main APP GUI
-    def __init__(self, master):
-        self.master = master
-        tk.Frame.__init__(self, self.master)
+    #Initializing the start of the Window
+    def __init__(self, parent):
+        self.parent = parent
+        tk.Frame.__init__(self, self.parent)
         self.configure_Gui()
+        self.TabControls = TabsControl(self)
+        self.TabControls.pack(expand=1, fill = 'both')
         #self.Widgets()
-
-    #Windows Basic GUI
+        
+    #Windows
     def configure_Gui(self):
-        self.master.title("JobSeeker Organizer") #Window Name
-        self.master.iconphoto(True, tk.PhotoImage(file='O:\Python\Job Seeker Organization\Images\Icon_Organizer.png')) #Window Icon set to True applies to all windows
-        self.master.minsize(500,400)
-        self.master.config(bg="red")
+        self.parent.title("JobSeeker Organizer") #Window Name
+        self.parent.iconphoto(True, tk.PhotoImage(file='O:\Python\Job Seeker Organization\Images\Icon_Organizer.png')) #Window Icon set to True applies to all windows
+        self.parent.minsize(500,400)
+        #self.parent.bg("ADD8E6")
 
     #def Widgets(self):
 
+        
+#To return the result
 if __name__ == "__main__":
     root = tk.Tk()
-    #App = MainApplication(root)
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop() #Infinite Loop
